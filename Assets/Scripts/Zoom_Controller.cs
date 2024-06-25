155b3424f220a7debad0229cd38f1cb169113909
KadirToptas <kdrrtptss@gmail.com>
2024-06-25 17:47:25 +0300

    Zoom

--- Assets/Scripts/Zoom_Controller.cs
+++ Assets/Scripts/Zoom_Controller.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Zoom_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Camera _cam;
+
+    [Header("Position")]
+    
+    [SerializeField] private Vector3 _startPosition;
+
+    [SerializeField] private Vector3 _endPos;
+
+    [SerializeField] [Range(0,20)] private float _lerpTime;
+
+    [Header("Rotation")] 
+    [SerializeField] private Vector3 startRotation;
+
+    [SerializeField] private Vector3 endRotation;
     void Start()
     {
-        
+        _cam = Camera.main;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            UpdateCameraPosition(_endPos);
+            UpdateCameraRotation(endRotation);
+        }
+        else
+        {
+            UpdateCameraPosition(_startPosition);
+
+            UpdateCameraRotation(startRotation);
+        }
+    }
+
+    private void UpdateCameraPosition(Vector3 _target)
+    {
+        _cam.transform.position = Vector3.Lerp(_cam.transform.position,_target,_lerpTime * Time.deltaTime);   
+    }
+
+    private void UpdateCameraRotation(Vector3 _target)
+    {
+        _cam.transform.rotation = Quaternion.Slerp(_cam.transform.rotation, Quaternion.Euler(_target), _lerpTime*Time.deltaTime);
     }
 }
